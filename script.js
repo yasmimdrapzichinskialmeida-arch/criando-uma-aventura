21aae235dfe0b0845526f0c87a444624f4aef0b8
yasmimdrapzichinskialmeida-arch <yasmim.drapzichinski.almeida@escola.pr.gov.br>
2025-12-12 15:11:26 -0300

    ativo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo')
+        const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
